3eafecd2c05f73a87f2d9348cc80d7e73e18bf45
Haoyan Li <haoyanli234@gmail.com>
2025-12-26 15:39:36 -0500

    dragging works now

--- cameratest.py
+++ cameratest.py
@@ -1,27 +1,41 @@
-import cv2, mediapipe, mouse, ctypes
+import cv2, mediapipe, pyautogui, mouse 
 
 camera = cv2.VideoCapture(0)
 
+SPEED = 2
+
 # get monitor size 
-user32 = ctypes.windll.user32
-screenx, screeny = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+screenx, screeny = pyautogui.size()
+
+last_x,last_y, current_x, current_y = 0,0,0,0
 
 def display_result(result, output_image, timestamp_ms):
+    global last_x,last_y, current_x, current_y
     try:
-        current_gesture = result.gestures[0][0].category_name
-        # print(result)
-        if current_gesture == "Pointing_Up":
-            mouse.move(result.hand_landmarks[0][8].x * (screenx), result.hand_landmarks[0][8].y * (screeny), absolute = True, duration = 0)
-            print(result.hand_landmarks[0][8].x * (screenx * 2))
-        if current_gesture == "Closed_Fist":
-            mouse.click()
-            # todo: add drag
+        index = 0
+        for gesture in result.gestures:
+            current_gesture = gesture[0].category_name
+            current_hand = index # flip-flop the checking between each hand
+
+            current_x, current_y = result.hand_landmarks[current_hand][8].x, result.hand_landmarks[current_hand][8].y
+            if current_gesture == "Pointing_Up":
+                mouse.move((current_x - last_x) * screenx * SPEED, (current_y - last_y) * screeny * SPEED, absolute=False, duration=0)
+            if current_gesture == "Closed_Fist":
+                if mouse.is_pressed():
+                    mouse.move((current_x - last_x) * screenx * SPEED, (current_y - last_y) * screeny * SPEED, absolute=False, duration=0)
+                else:
+                    pyautogui.mouseDown()
+            if current_gesture != "Closed_Fist" and mouse.is_pressed(): 
+                pyautogui.mouseUp()
+            last_x, last_y = current_x, current_y
+            index += 1
     except IndexError:
         pass
 
 recognizer_options = mediapipe.tasks.vision.GestureRecognizerOptions(
     base_options=mediapipe.tasks.BaseOptions(model_asset_path='gesture_recognizer.task'),
     running_mode=mediapipe.tasks.vision.RunningMode.LIVE_STREAM,
+    num_hands=2,
     result_callback=display_result)
 
 recognizer = mediapipe.tasks.vision.GestureRecognizer.create_from_options(recognizer_options)
@@ -47,7 +61,6 @@ while True:
 
     if cv2.waitKey(50) & 0xFF == ord('q'):
         break
-        quit()
 
     timer += 1
 
